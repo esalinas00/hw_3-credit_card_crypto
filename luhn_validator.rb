9e9fc923a00ed503b07f0bdfb153947d58b65107
John-Lin <linton.tw@gmail.com>
2016-03-07 03:00:24 +0800

    optimization for benchmark

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,14 +4,17 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)[0..-2]
-    if nums_a.length & 1 != 0
-      pos_mul_2 = nums_a.each_with_index.map{ |d, i| i & 1 != 0 ? d : d << 1 }
+    nums_a = number.to_s.chars.map(&:to_i)
+    popped_last_nums_a = nums_a[0..-2]
+
+    if popped_last_nums_a.length.even?
+      pos_mul_2 = popped_last_nums_a.each_with_index.map{ |d, i| i.even? ? d << 1 : d }
     else
-      pos_mul_2 = nums_a.each_with_index.map{ |d, i| i & 1 != 0 ? d << 1 : d }
+      pos_mul_2 = popped_last_nums_a.each_with_index.map{ |d, i| i.odd? ? d << 1 : d }
     end
-    digit_might_be_2 = 10 - (pos_mul_2.map{ |d| d > 9 ? d - 10 + 1 : d }.inject(0, :+) % 10)
+
+    digit_might_be_2 = 10 - (pos_mul_2.map{ |d| d > 9 ? d - 10 + 1 : d }.reduce(:+) % 10)
     digit_might_be_2 == 10 ? next_check_digit = 0 : next_check_digit = digit_might_be_2
-    number.to_s.chars.map(&:to_i).pop == next_check_digit
+    nums_a.last == next_check_digit
   end
 end
