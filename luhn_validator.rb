320a76914f7d4d2d0d76de63e70a0710940cfafa
John-Lin <linton.tw@gmail.com>
2016-03-07 02:21:25 +0800

    remove comment and print messages

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,14 +5,9 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)[0..-2]
-
     if nums_a.length & 1 != 0
-      # odd
-      puts "Odd length : #{nums_a.length}"
       pos_mul_2 = nums_a.each_with_index.map{ |d, i| i & 1 != 0 ? d : d << 1 }
     else
-      # even
-      puts "Even length : #{nums_a.length}"
       pos_mul_2 = nums_a.each_with_index.map{ |d, i| i & 1 != 0 ? d << 1 : d }
     end
     digit_might_be_2 = 10 - (pos_mul_2.map{ |d| d > 9 ? d - 10 + 1 : d }.inject(0, :+) % 10)
