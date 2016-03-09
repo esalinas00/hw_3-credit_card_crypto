2824d5bc0ce6720c9bb510da254eee0a5cae9590
John-Lin <linton.tw@gmail.com>
2016-03-09 12:49:40 +0800

    create a hash that converts the instance variables in a JSON string format

--- credit_card.rb
+++ credit_card.rb
@@ -1,5 +1,6 @@
 require_relative './luhn_validator.rb'
 require 'json'
+require 'openssl'
 
 class CreditCard
   include LuhnValidator
@@ -11,12 +12,19 @@ class CreditCard
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
     @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      'Name' => @owner,
+      'Card_Number' => @number,
+      'Expiration_Date' => @expiration_date,
+      'Credit_Card_Network' => @credit_network
     }.to_json
   end
 
@@ -28,14 +36,21 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    c = JSON.parse(card_s)
+    CreditCard.new(
+      c['Card_Number'],
+      c['Expiration_Date'],
+      c['Name'],
+      c['Credit_Card_Network']
+    )
   end
 
   # return a hash of the serialized credit card object
   def hash
-    # TODO: implement this method
-    #   - Produce a hash (using default hash method) of the credit card's
-    #     serialized contents.
-    #   - Credit cards with identical information should produce the same hash
+    # TODO: Produce a hash (using default hash method) of the credit card's
+    #       serialized contents.
+    #       Credit cards with identical information should produce the same hash.
+    to_s.hash
   end
 
   # return a cryptographically secure hash
@@ -43,5 +58,7 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 from openssl to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    sha256 = OpenSSL::Digest::SHA256.new
+    sha256.digest(to_s).unpack('H*')
   end
 end
