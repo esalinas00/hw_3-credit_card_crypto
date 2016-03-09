6cd2ec03c35e18add4b8b592ce9b0dbb830cef29
esalinas00 <esalinas@sd202096.HUNG.ab.nthu.edu.tw>
2016-03-09 16:57:14 +0800

    Add LuhnValidato validate_checksum code

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,21 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    check_digit = nums_a.pop
+    nums_a.reverse!.map!.with_index do |n,i|
+  	  if i%2 == 0 
+	    n2 = n*2
+	    ((n2%10)+(n2/10))
+  	  else
+  	    n
+  	  end
+  	end
+  	
+  	mod = nums_a.reduce(&:+)%10
+  	if mod == 0
+      mod == check_digit
+  	else
+  	  10 - mod == check_digit
+  	end
   end
 end
