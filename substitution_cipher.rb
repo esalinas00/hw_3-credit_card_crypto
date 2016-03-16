c9e0010dde1634bc3519b0f9a3ce1ce843ae557f
Che-Wei Lin <linton.tw@gmail.com>
2016-03-17 02:28:07 +0800

    Add Caesar Cipher and Permutation Cipher solution

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,6 +7,22 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+
+      # readable version
+      # document.to_s.chars.map do |c|
+      #   if c.ord + key > 126
+      #     new_ord = 31 + (c.ord + key) - 126
+      #   else
+      #     new_ord = c.ord + key
+      #   end
+      # end
+
+      document
+        .to_s
+        .chars
+        .map{ |c| c.ord + key > 126 ? 31 + (c.ord + key) - 126 : c.ord + key }
+        .map{ |o| o.chr }
+        .join
     end
 
     # Decrypts String document using integer key
@@ -16,6 +32,22 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+
+      # readable version
+      # document.to_s.chars.map do |c|
+      #   if c.ord - key < 32
+      #     new_ord = 126 + (c.ord - key) - 31
+      #   else
+      #     new_ord = c.ord - key
+      #   end
+      # end
+
+      document
+        .to_s
+        .chars
+        .map{ |c| c.ord - key < 32 ? 126 + (c.ord - key) - 31 : c.ord - key }
+        .map{ |o| o.chr }
+        .join
     end
   end
 
@@ -27,6 +59,18 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      all_ascii_ord = [*32..126]
+
+      new_ord = all_ascii_ord.shuffle(random: Random.new(key))
+
+      all_ascii_to_new_ord = Hash[all_ascii_ord.zip(new_ord)]
+
+      document
+        .to_s
+        .chars
+        .map{ |c| all_ascii_to_new_ord[c.ord] }
+        .map{ |o| o.chr }
+        .join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +80,18 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      all_ascii_ord = [*32..126]
+
+      new_ord = all_ascii_ord.shuffle(random: Random.new(key))
+
+      new_ord_to_all_ascii = Hash[new_ord.zip(all_ascii_ord)]
+
+      document
+        .to_s
+        .chars
+        .map{ |c| new_ord_to_all_ascii[c.ord] }
+        .map{ |o| o.chr }
+        .join
     end
   end
 end
