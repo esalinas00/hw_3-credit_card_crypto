e339b31005f16a2defc096c250b894c4942c4267
esalinas00 <efsalinas00@gmail.com>
2016-03-17 15:52:29 +0800

    Add Double Transposition Cypher solution

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,3 +1,10 @@
+class Array
+  def unshuffle(random:)
+    transformed_order = (0...length).to_a.shuffle!(random: random)
+    sort_by.with_index{|_, i| transformed_order[i]}
+  end
+end
+
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
     # TODO: FILL THIS IN!
@@ -7,9 +14,29 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+    
+    array_a = create_matrix(document)
+    #shuffle the array
+    array_a.shuffle!(random: Random.new(key))
+    array_a.each { |row| row.shuffle!(random: Random.new(key)) }
+    array_a.join("")
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
+    array_a = create_matrix(ciphertext)    
+    array_a.map! { |row| row.unshuffle(random: Random.new(key)) }
+    array_a = array_a.unshuffle(random: Random.new(key))
+    array_a.join("")
+  end
+
+  def self.create_matrix(document)
+    document = document.to_s.chars
+    a_len = (document.length ** 0.5).ceil
+    array_a = Array.new(a_len) do |i| 
+      i = Array.new(a_len) do |j|  
+        document[0] == nil ? ('a'..'z').to_a[rand(26)] : document.shift
+      end
+    end
   end
 end
