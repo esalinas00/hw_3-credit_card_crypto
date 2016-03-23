4ede8640ed49c5a5ddd9f72060c692fd3f1b7234
Che-Wei Lin <linton.tw@gmail.com>
2016-03-24 02:14:00 +0800

    Add AES tests

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
+require_relative '../aes_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -51,6 +52,20 @@ describe 'Test card info encryption' do
       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
       dec = DoubleTranspositionCipher.decrypt(enc, @key)
       dec[0,@cc.to_s.length].must_equal @cc.to_s
+    end
+  end
+
+  describe 'Using AES cipher' do
+    it 'should encrypt card information' do
+      enc = AesCipher.encrypt(@cc, @key)
+      enc.wont_equal @cc.to_s
+      enc.wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      enc = AesCipher.encrypt(@cc, @key)
+      dec = AesCipher.decrypt(enc, @key)
+      dec.must_equal @cc.to_s
     end
   end
 
